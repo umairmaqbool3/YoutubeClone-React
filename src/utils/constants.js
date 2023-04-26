aa6c0a7366c1b64b5a8fae6f89163ddc346ddcbf
Umair Maqbool <20332318+umairmaqbool3@users.noreply.github.com>
2023-04-26 20:27:01 +0500

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -9,7 +9,7 @@ import { ImNewspaper } from "react-icons/im";
 import { GiDiamondTrophy, GiEclipse } from "react-icons/gi";
 import { RiLightbulbLine, RiFeedbackLine } from "react-icons/ri";
 import { FiSettings, FiHelpCircle } from "react-icons/fi";
-
+// used for menu
 export const categories = [
     { name: "New", icon: <AiFillHome />, type: "home" },
     { name: "Trending", icon: <MdLocalFireDepartment />, type: "category" },
