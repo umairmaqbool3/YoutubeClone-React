2b884e55ee23289607a67ccf218192a33e7b1549
Umair Maqbool <20332318+umairmaqbool3@users.noreply.github.com>
2023-08-03 22:25:45 +0500

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -10,6 +10,7 @@ const options = {
     }
 };
 
+// method to fetch api data
 export const fetchDataFromApi = async (url) => {
     const {data} = await axios.get(`${BASE_URL}/${url}`, options)
     return data
