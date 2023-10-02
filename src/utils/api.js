ee39aece2d61364fcd6bcad2a406c220a1ce9e55
Umair Maqbool <20332318+umairmaqbool3@users.noreply.github.com>
2023-10-02 23:48:34 +0500

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -10,7 +10,7 @@ const options = {
     }
 };
 
-// this method fetch data from rapid api
+
 export const fetchDataFromApi = async (url) => {
     const {data} = await axios.get(`${BASE_URL}/${url}`, options)
     return data
